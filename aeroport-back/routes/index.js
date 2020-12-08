9607627c05d9363c581aa0af94d84e9db2eaf5cd
Jessy-Youss <64401028+Jessy-Youss@users.noreply.github.com>
2020-12-08 14:22:06 +0100

    ok

--- aeroport-back/routes/index.js
+++ aeroport-back/routes/index.js
@@ -6,7 +6,7 @@ const Readline = require('@serialport/parser-readline');
 const port = new SerialPort('COM24', { baudRate: 9600 });
 const parser = port.pipe(new Readline({ delimiter: '\n' }));
 
-var mysql = require ('../../aeroport-front/public/javascripts/BDD')
+var mysql = require ('./BDD')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
